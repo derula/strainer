3466e86478eba51760c103517e8134623df5a866
Simon Paridon <Simon.Paridon@uglyhorst.de>
2019-12-14 15:12:37 +0100

    Encrypt saved passwords.

--- strainer/config.py
+++ strainer/config.py
@@ -1,5 +1,10 @@
-import os
+import base64
 import configparser
+import os
+import uuid
+import struct
+
+from cryptography import fernet
 
 from .types import Account
 
@@ -20,14 +25,15 @@ class Accounts:
         self._filename = filename
         self._parser = configparser.ConfigParser()
         self._parser.read(filename)
+        self._key = self._get_key()
 
     @property
     def all(self):
         for section in self._parser.sections():
-            yield Account.unserialize(dict(self._parser.items(section), name=section))
+            yield Account.unserialize(dict(self._parser.items(section), name=section), self._key)
 
     def add(self, account):
-        self._parser[account.name] = account.serialize()
+        self._parser[account.name] = account.serialize(self._key)
         self._save()
 
     def update(self, old_name, account):
@@ -41,6 +47,20 @@ class Accounts:
     def _save(self):
         with open(self._filename, 'w') as f:
             self._parser.write(f)
+
+    def _get_key(self):
+        path = os.path.join(config_dir, 'key.bin')
+        try:
+            with open(path, 'rb') as f:
+                key = f.read()
+        except FileNotFoundError:
+            key = os.urandom(32)
+            with open(path, 'wb') as f:
+                f.write(key)
+        key = bytearray(key)
+        for i, b in enumerate((struct.pack('Q', uuid.getnode())[:-2] * 6)[:-4]):
+            key[i] ^= b
+        return fernet.Fernet(base64.urlsafe_b64encode(key))
 
 config_dir = _get_config_dir()
 accounts = Accounts(os.path.join(config_dir, 'accounts.ini'))
--- strainer/types.py
+++ strainer/types.py
@@ -1,3 +1,4 @@
+from base64 import b64decode, b64encode
 from enum import Enum, auto
 from typing import NamedTuple, Optional
 
@@ -18,18 +19,25 @@ class Account(NamedTuple):
     authmech: Optional[str]
 
     @classmethod
-    def unserialize(cls, values):
+    def unserialize(cls, values, pw_key=None):
         values['port'] = _get_int(values, 'port', 4190)
         values['starttls'] = bool(_get_int(values, 'starttls', 0))
         values.setdefault('authmech', None)
+        if pw_key is not None:
+            try:
+                values['passwd'] = pw_key.decrypt(b64decode(values['passwd'])).decode('utf-8')
+            except Exception:
+                values['passwd'] = ''
         return cls(**values)
 
-    def serialize(self):
+    def serialize(self, pw_key=None):
         result = self._asdict()
         del result['name']
         if self.authmech is None:
             del result['authmech']
         result['starttls'] = int(self.starttls)
+        if pw_key is not None:
+            result['passwd'] = b64encode(pw_key.encrypt(self.passwd.encode('utf-8'))).decode('ascii')
         return result
 
 class TreeItemStatus(Enum):
