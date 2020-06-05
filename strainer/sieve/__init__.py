aaa5066ac73f8d1da2f1ca824c8627802f5740e6
Simon Paridon <Simon.Paridon@uglyhorst.de>
2020-06-05 23:27:05 +0200

    Monkeypatch tonioo/sievelib#90.

--- strainer/sieve/__init__.py
+++ strainer/sieve/__init__.py
@@ -1,1 +1,8 @@
+from sievelib.commands import ExistsCommand
+
+# Patch sievelib bug #90
+ExistsCommand.args_definition[0]['type'].append('string')
+
 from .connection import SieveConnectionQueue, SieveErrorChecker
+
+__all__ = ('SieveConnectionQueue', 'SieveErrorChecker')
