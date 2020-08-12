53e75960fd14023e219238f02caf6d8f47b1328a
Simon Paridon <Simon.Paridon@uglyhorst.de>
2020-08-12 14:59:30 +0200

    Added FindMixin implementation.

--- strainer/widgets/base.py
+++ strainer/widgets/base.py
@@ -1,6 +1,10 @@
+from PyQt5.QtWidgets import QVBoxLayout, QWidget
 from dataclasses import dataclass, field
 
-__all__ = ('Menu', 'MenuMixin')
+from ..controls import FindBar
+from ..types import FindOptions, FindQuery
+
+__all__ = ('Menu', 'MenuMixin', 'Find', 'FindMixin')
 
 
 @dataclass(frozen=True)
@@ -28,3 +32,54 @@ class MenuMixin:
 
     def updateMenu(self):
         self._menu.update(self)
+
+
+@dataclass(frozen=True)
+class Find:
+    action: type
+    options: FindOptions
+
+
+class FindMixin:
+    _find: Find
+
+    def __init__(self, parent):
+        super().__init__(parent.window())
+        findBar = FindBar(parent.window(), self._find.options)
+        widget = QWidget(parent)
+        widget.setLayout(QVBoxLayout())
+        widget.layout().setContentsMargins(0, 0, 0, 0)
+        widget.layout().addWidget(findBar)
+        widget.layout().addWidget(self)
+        findBar.findFirst.connect(self.findFirst)
+        findBar.findNext.connect(self.findNext)
+        findBar.cancelFind.connect(self.cancelFind)
+        self.window().action(self._find.action).triggered.connect(lambda: findBar.show(self.selectedText()))
+        self._find = None
+
+    def canFindNext(self):
+        return self._find is not None
+
+    def findFirst(self, query: FindQuery):
+        if not self.canFindNext():
+            self.cancelFind()
+        self._find = query
+        self._findFirst(query)
+
+    def findNext(self):
+        if not self.canFindNext():
+            raise RuntimeError('Cannot findNext before findFirst.')
+        self._findNext(self._find)
+
+    def cancelFind(self):
+        self._find = None
+        self._cancelFind()
+
+    def _findFirst(self, query: FindQuery):
+        self._findNext(query)
+
+    def _findNext(self, query: FindQuery):
+        raise NotImplementedError('Abstract method _findNext must be implemented by all subclasses.')
+
+    def _cancelFind(self):
+        raise NotImplementedError('Abstract method _findNext must be implemented by all subclasses.')
