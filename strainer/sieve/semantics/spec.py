9a8585d0b78a061a0e48958873e32b37ff6d9a02
Simon Paridon <Simon.Paridon@uglyhorst.de>
2021-05-14 14:04:35 +0200

    Avoid weird tags.Tags construct.

--- strainer/sieve/semantics/spec.py
+++ strainer/sieve/semantics/spec.py
@@ -1,14 +1,16 @@
 from __future__ import annotations
+
 from dataclasses import dataclass, field
 from typing import Mapping, Optional, Sequence
 
 from . import tags
+from .tags import Tags
 
 
 @dataclass
 class CommandSpec:
     positional_args: Sequence[str] = ()
-    tags: tags.Tags = field(default_factory=tags.Tags)
+    tags: Tags = field(default_factory=Tags)
     test_type: Optional[str] = None
     must_follow: Optional[Sequence[bytes]] = None
     has_block: bool = False
@@ -23,7 +25,7 @@ class Capability:
 
 document_start = object()
 block_start = object()
-core_tags = tags.Tags(tags.over_under, tags.comparator, tags.match_type, tags.address_part)
+core_tags = Tags(tags.over_under, tags.comparator, tags.match_type, tags.address_part)
 core_commands = {
     b'keep': CommandSpec(),
     b'stop': CommandSpec(),
@@ -41,18 +43,17 @@ core_tests = {
     b'allof': CommandSpec(test_type='test_list'),
     b'anyof': CommandSpec(test_type='test_list'),
     b'exists': CommandSpec(('string_list',)),
-    b'size': CommandSpec(tags=tags.Tags(tags.over_under)),
-    b'header': CommandSpec(('string_list', 'string_list'), tags.Tags(tags.comparator, tags.match_type)),
-    b'address': CommandSpec(('string_list', 'string_list'), tags.Tags(tags.comparator, tags.match_type,
-                                                                      tags.address_part)),
+    b'size': CommandSpec(tags=Tags(tags.over_under)),
+    b'header': CommandSpec(('string_list', 'string_list'), Tags(tags.comparator, tags.match_type)),
+    b'address': CommandSpec(('string_list', 'string_list'), Tags(tags.comparator, tags.match_type, tags.address_part)),
 }
 capabilities = {
     b'fileinto': Capability(commands={b'fileinto': CommandSpec(('string',))}),
     b'envelope': Capability(tests={b'envelope': CommandSpec(('string_list', 'string_list'),
-                                                            tags.Tags(tags.comparator, tags.match_type,
-                                                                      tags.address_part))}),
+                                                            Tags(tags.comparator, tags.match_type,
+                                                                 tags.address_part))}),
     b'body': Capability(tests={b'body': CommandSpec(('string_list',),
-                                                    tags.Tags(tags.comparator, tags.match_type, tags.body_transform))}),
+                                                    Tags(tags.comparator, tags.match_type, tags.body_transform))}),
     b'convert': Capability(commands={b'convert': CommandSpec(('string', 'string', 'string_list'))},
                            tests={b'convert': CommandSpec(('string', 'string', 'string_list'))}),
 }
